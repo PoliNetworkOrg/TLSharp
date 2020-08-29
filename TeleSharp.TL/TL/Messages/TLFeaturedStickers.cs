0da0b9c9535faf75cc2b13d850fd01d88f512890
Federico Armellini <armellini.federico@gmail.com>
2020-08-29 15:22:49 +0200

    update tl schema

--- TeleSharp.TL/TL/Messages/TLFeaturedStickers.cs
+++ TeleSharp.TL/TL/Messages/TLFeaturedStickers.cs
@@ -7,41 +7,44 @@ using System.Threading.Tasks;
 using TeleSharp.TL;
 namespace TeleSharp.TL.Messages
 {
-    [TLObject(-123893531)]
+	[TLObject(-1230257343)]
     public class TLFeaturedStickers : TLAbsFeaturedStickers
     {
         public override int Constructor
         {
             get
             {
-                return -123893531;
+                return -1230257343;
             }
         }
 
-        public int Hash { get; set; }
-        public TLVector<TLAbsStickerSetCovered> Sets { get; set; }
-        public TLVector<long> Unread { get; set; }
+             public int Hash {get;set;}
+     public int Count {get;set;}
+     public TLVector<TLAbsStickerSetCovered> Sets {get;set;}
+     public TLVector<long> Unread {get;set;}
 
 
-        public void ComputeFlags()
-        {
-
-        }
+		public void ComputeFlags()
+		{
+			
+		}
 
         public override void DeserializeBody(BinaryReader br)
         {
             Hash = br.ReadInt32();
-            Sets = (TLVector<TLAbsStickerSetCovered>)ObjectUtils.DeserializeVector<TLAbsStickerSetCovered>(br);
-            Unread = (TLVector<long>)ObjectUtils.DeserializeVector<long>(br);
+Count = br.ReadInt32();
+Sets = (TLVector<TLAbsStickerSetCovered>)ObjectUtils.DeserializeVector<TLAbsStickerSetCovered>(br);
+Unread = (TLVector<long>)ObjectUtils.DeserializeVector<long>(br);
 
         }
 
         public override void SerializeBody(BinaryWriter bw)
         {
-            bw.Write(Constructor);
+			bw.Write(Constructor);
             bw.Write(Hash);
-            ObjectUtils.SerializeObject(Sets, bw);
-            ObjectUtils.SerializeObject(Unread, bw);
+bw.Write(Count);
+ObjectUtils.SerializeObject(Sets,bw);
+ObjectUtils.SerializeObject(Unread,bw);
 
         }
     }
