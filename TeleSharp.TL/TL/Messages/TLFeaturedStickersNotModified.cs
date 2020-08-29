e7292865ad5ed19cc56ca5787c5e95cc50b6b67f
Federico Armellini <armellini.federico@gmail.com>
2020-08-29 15:07:39 +0200

    update tl schema

--- TeleSharp.TL/TL/Messages/TLFeaturedStickersNotModified.cs
+++ TeleSharp.TL/TL/Messages/TLFeaturedStickersNotModified.cs
@@ -7,32 +7,35 @@ using System.Threading.Tasks;
 using TeleSharp.TL;
 namespace TeleSharp.TL.Messages
 {
-    [TLObject(82699215)]
+	[TLObject(-958657434)]
     public class TLFeaturedStickersNotModified : TLAbsFeaturedStickers
     {
         public override int Constructor
         {
             get
             {
-                return 82699215;
+                return -958657434;
             }
         }
 
+             public int Count {get;set;}
 
 
-        public void ComputeFlags()
-        {
-
-        }
+		public void ComputeFlags()
+		{
+			
+		}
 
         public override void DeserializeBody(BinaryReader br)
         {
+            Count = br.ReadInt32();
 
         }
 
         public override void SerializeBody(BinaryWriter bw)
         {
-            bw.Write(Constructor);
+			bw.Write(Constructor);
+            bw.Write(Count);
 
         }
     }
